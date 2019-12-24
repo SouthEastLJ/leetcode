f8113c2adaad514ef9d931d0f7b64eae45039fd8
liujian <sdycliujian@163.com>
2019-12-25 00:08:47 +0800

    leetcode47 second commit

--- src/leetcode47.java
+++ src/leetcode47.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -16,8 +19,28 @@ public class leetcode47 {
     }
 
     public List<List<Integer>> permuteUnique(int []nums){
-        //d
-
-        return null;
+        if(nums.length==0){
+            return null;
+        }
+        List<List<Integer>> result = new LinkedList<>();
+        Arrays.sort(nums);
+        backtrack(result,new ArrayList<>(),nums,new boolean[nums.length]);
+        return result;
     }
+
+    private void backtrack(List<List<Integer>> list,List<Integer> tempList, int []nums,boolean []used){
+        if(tempList.size() == nums.length){
+            list.add(new ArrayList<>(tempList));
+        }else{
+            for(int i=0;i<nums.length;++i){
+                if(used[i] || i>0 && nums[i]==nums[i-1] && !used[i-1]) continue;
+                used[i] = true;
+                tempList.add(nums[i]);
+                backtrack(list,tempList,nums,used);
+                used[i] = false;
+                tempList.remove(tempList.size()-1);
+            }
+        }
+    }
+
 }
