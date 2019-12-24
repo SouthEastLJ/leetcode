97bae6092fb0ffe2bdf33566d98113feb98e606d
liujian <sdycliujian@163.com>
2019-12-24 23:50:20 +0800

    leetcode47 first commit

--- src/leetcode47.java
+++ src/leetcode47.java
@@ -1,6 +1,23 @@
-/**
- *
- */
+import java.util.List;
 
+/**
+ * given a collection of numbers that might contain duplicates, return all possible unique permutations
+ */
 public class leetcode47 {
+    public static void main(String []args){
+        int []nums = {1,1,2};
+        List<List<Integer>> result = new leetcode47().permuteUnique(nums);
+        for(List<Integer> list:result){
+            for(Integer j:list){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public List<List<Integer>> permuteUnique(int []nums){
+        //d
+
+        return null;
+    }
 }
